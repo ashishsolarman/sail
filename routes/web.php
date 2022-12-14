590aa25732f2972f469f1d1f4915533a135b7cd8
Ashish tomar <ashish@solarman.in>
2022-12-14 17:40:25 +0530

    This is a new commit

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ use Illuminate\Support\Facades\Route;
      
       use App\Http\Controllers\Auth\LoginController;
 
-      //arun
-
 Route::get('/', function () {
     return view('auth.login');
 });
